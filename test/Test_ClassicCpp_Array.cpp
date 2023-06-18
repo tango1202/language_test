2ce0081e6623fc8f985d3233d8be310f07d82f39
tango1202 <ws1202@gmail.com>
2023-06-19 08:07:32 +0900

    array

--- test/Test_ClassicCpp_Array.cpp
+++ test/Test_ClassicCpp_Array.cpp
@@ -8,7 +8,9 @@ TEST(TestClassicCpp, Array) {
     {
         // 자료형 재정의
         typedef int MyArray[5]; 
-        MyArray arr = {}; // int arr[5]; 와 동일 
+        MyArray arr; // int arr[5]; 와 동일 
+        arr[0] = 10;// 첫번째 요소에 값 대입
+        EXPECT_TRUE(arr[0] == 10);
     }
     {
         int* p = new int[5]; // int[5] 배열 생성
