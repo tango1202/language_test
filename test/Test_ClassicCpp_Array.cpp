f30d2205248a6729bfa4a5799dcf12e248d07aaa
tango1202 <ws1202@gmail.com>
2023-07-02 16:12:24 +0900

    타입

--- test/Test_ClassicCpp_Array.cpp
+++ test/Test_ClassicCpp_Array.cpp
@@ -2,10 +2,10 @@
 
 TEST(TestClassicCpp, Array) {
     // ----
-    // typedef로 자료형 재정의
+    // typedef로 타입 재정의
     // ----
     {
-        // 자료형 재정의
+        // 타입 재정의
         typedef int MyArray[5]; 
         MyArray arr; // int arr[5]; 와 동일 
         arr[0] = 10; // 첫번째 요소에 값 대입
