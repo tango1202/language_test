6d8ec9dfb3dbf4a6e5f58c21e28052c1f4191ea5
정원석 <133472501+tango1202@users.noreply.github.com>
2023-08-17 12:58:21 +0900

    Update Test_ClassicCpp_TemplateParameterArgument.cpp

--- test/Test_ClassicCpp_TemplateParameterArgument.cpp
+++ test/Test_ClassicCpp_TemplateParameterArgument.cpp
@@ -193,7 +193,7 @@ TEST(TestClassicCpp, TemplateParameterArgument) {
         using namespace Parameter_5;
         B<A> b; 
         b.m_U1.m_X = 10;
-        b.m_U2.m_Y = 20;
+        b.m_U2.m_Y = 20; // A<T*> 버전으로 생성됩니다.
 
         EXPECT_TRUE(b.m_U1.m_X == 10);
         EXPECT_TRUE(b.m_U2.m_Y == 20);
