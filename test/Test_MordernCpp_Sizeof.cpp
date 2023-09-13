2a04e73723ed51b00a9494e293ba92216babe02f
wsjung <wsjung1202@gmail.com>
2023-09-14 08:17:58 +0900

    기타

--- test/Test_MordernCpp_Sizeof.cpp
+++ test/Test_MordernCpp_Sizeof.cpp
@@ -1,7 +1,5 @@
 #include "gtest/gtest.h" 
 
-    template <typename T>
-class A {};
 TEST(TestMordern, Sizeof) {
 
     class T {
@@ -12,10 +10,5 @@ TEST(TestMordern, Sizeof) {
     // C++03에서는 컴파일 오류
     // C++11부터 허용
     EXPECT_TRUE(sizeof(T::m_X) == sizeof(int));
-
-
-
-std::vector<A<int> > a; // C++03. 공백을 추가해야 했습니다.
-std::vector<A<int>> b; // C++11
 }
 
